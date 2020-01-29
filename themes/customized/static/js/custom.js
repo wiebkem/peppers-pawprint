ef7fbfda03747014de14e474188fbba464642bf5
Wiebke <wickoby@gmail.com>
2020-01-29 21:18:16 +0100

    Added the privacyOptOut id to not open it in a blank tab

--- themes/customized/static/js/custom.js
+++ themes/customized/static/js/custom.js
@@ -1,7 +1,7 @@
 (function() {
   var links = document.links;
   for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-    if (links[i].hostname != window.location.hostname) {
+    if (links[i].hostname != window.location.hostname && links[i].id != "privacyOptOut") {
       links[i].target = "_blank";
       links[i].setAttribute("rel", "noopener noreferrer");
     }
