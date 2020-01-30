eb7a388fa77e62e56a8eaf1ca3892a0dd60aa772
Wiebke <wickoby@gmail.com>
2020-01-30 20:28:44 +0100

    Updated the docs folder

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -7,34 +7,3 @@
     }
   }
 })();
-
-function enableGoogleAnalytics() {
-  // check the do not track option from the browser
-  var dnt = (navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack);
-  var doNotTrack = (dnt == "1" || dnt == "yes");
-  if (!doNotTrack) {
-      window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-      if (window.sessionStorage) {
-          var GA_SESSION_STORAGE_KEY = 'ga:clientId';
-          ga('create', 'UA-154765901-1', {
-              'storage': 'none',
-              'clientId': sessionStorage.getItem(GA_SESSION_STORAGE_KEY)
-          });
-          ga(function(tracker) {
-              sessionStorage.setItem(GA_SESSION_STORAGE_KEY, tracker.get('clientId'));
-          });
-      }
-      ga('set', 'anonymizeIp', true);
-      ga('send', 'pageview');
-  }
-}
-
-function disableGoogleAnalytics() {
-  var gaProperty = '{{ .Site.GoogleAnalytics }}';
-  var disableStr = 'ga-disable-' + gaProperty;
-  if (document.cookie.indexOf(disableStr + '=true') > -1) {
-      window[disableStr] = true;
-  }
-  document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
-  window[disableStr] = true;
-}
