d47806d735bbe93fc6e7e7170bdfb6cd93a525f9
Wiebke <wickoby@gmail.com>
2020-01-30 20:41:56 +0100

    Updated the custom js

--- themes/customized/static/js/custom.js
+++ themes/customized/static/js/custom.js
@@ -12,7 +12,7 @@
 // Opt-out function
 function gaOptout() {
   var gaProperty = 'UA-154765901-1';
-  var disableStr = 'ga-disable-UA-154765901-1' + gaProperty;
+  var disableStr = 'ga-disable-' + gaProperty;
   document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
   window[disableStr] = true;
 }
