575eea5412df93c326a2061b3b30b47022993c0d
Wiebke <wickoby@gmail.com>
2020-01-30 20:34:23 +0100

    Updated the docs folder

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -7,3 +7,12 @@
     }
   }
 })();
+
+
+// Opt-out function
+function gaOptout() {
+  var gaProperty = 'UA-154765901-1';
+  var disableStr = 'ga-disable-UA-154765901-1' + gaProperty;
+  document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+  window[disableStr] = true;
+}
