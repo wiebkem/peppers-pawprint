30938fcf92784aa3922f9c8cf7caaaf83aa5c80c
Wiebke <wickoby@gmail.com>
2020-04-18 08:57:15 +0200

    Updated the search query and edited the search result layout

--- themes/customized/static/js/search.js
+++ themes/customized/static/js/search.js
@@ -18,13 +18,33 @@ function renderSearchResults(results){
         results.forEach(result => {
         
             // create result item
-            var article = document.createElement('article')
-            article.innerHTML = `
-            <a href="${result.ref}"><h3 class="title">${documents[result.ref].title}</h3></a>
-            <p><a href="${result.ref}">${result.ref}</a></p>
-            <br>
+            var item = document.createElement('div');
+            item.className = "col-xs-12 col-sm-12 col-md-6";
+            item.innerHTML = `
+            <div class="post">
+                <p class="post-header-title"><a href="${result.ref}" title="">${documents[result.ref].title}</a></p>
+                <div class="post-header">
+                    <time class="date" datetime="2014-01-14T11:24">${documents[result.ref].date}</time>
+                    <div class="custom-line">
+                        <div id="hexagon"></div>
+                    </div>
+                </div>
+
+                <a href="${result.ref}" title="">
+                    <div class="post-image">
+                        <img src="${documents[result.ref].imageUrl}" alt="post-image" title="post-image">
+                    </div>
+                </a>
+                
+                <div class="post-description">
+                    <a href="${result.ref}" title="">${documents[result.ref].description}</a>
+                </div>
+                <div class="post-button">
+                    <a href="${result.ref}" class="button">${documents[result.ref].buttonText}</a>
+                </div>
+            </div>
             `
-            searchResults.appendChild(article)
+            searchResults.appendChild(item);
         })
 
     // if results are empty
@@ -76,15 +96,21 @@ window.onload = function() {
             idx = lunr(function() {
                 this.ref('url')
                 this.field('title')
+                this.field('date')
+                this.field('imageUrl')
                 this.field('description')
                 this.field('content')
+                this.field('buttonText')
 
                 res.forEach(function(doc) {
                     this.add(doc)
                     documents[doc.url] = {
                         'title': doc.title,
+                        'date': doc.date,
+                        'imageUrl': doc.imageUrl,
                         'description': doc.description,
                         'content': doc.content,
+                        'buttonText': doc.buttonText,
                     }
                 }, this)
             })
